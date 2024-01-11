e7e8fe9a4a6dfdf4353e3495ea4a7e3c40bce3e8
Merch7x <aquamaingi@gmail.com>
2024-01-11 20:08:49 +0300

    Create class FileStorage

---
+++ models/engine/file_storage.py
@@ -0,0 +1,39 @@
+"""Createas a class that serializes instances to a JSON file
+and  deserializes JSON file to instances
+"""
+import json
+from pathlib import Path
+from models.base_model import BaseModel
+
+
+class FileStorage(BaseModel):
+    """Creates a File storage instance"""
+    __file_path = Path('~/Dev/Alx/Airbnb/AirBnB_clone/file.json')
+    __objects = {}
+
+    def all(self):
+        """Returns a dictionary"""
+        self.__objects = __class__.__objects
+        return self.__objects
+
+    def new(self, obj):
+        """Sets the __objects dict with a key <obj class name>.id"""
+        id_value = obj.get('id')
+        class_name = obj.get('__class__')
+        if id_value is not None and class_name is not None:
+            key = f"{class_name}.{id_value}"
+            FileStorage.__objects[key] = obj
+        else:
+            raise KeyError()
+
+    def save(self):
+        """Serializes a python object to json"""
+        with open(FileStorage.__file_path, 'a+') as file:
+            file.seek(0)  # allows for reading of the file
+            json.dump(FileStorage.__objects, file)
+
+    def reload(self):
+        """Deserializes json to python obj in __objects"""
+        if FileStorage.__file_path.exists():
+            with open(FileStorage.__file_path, 'r') as file:
+                FileStorage.new(json.load(file))
